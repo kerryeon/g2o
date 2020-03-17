6c7329c8d746ec2b16d93647fc402da08b1cf0ae
h <besqer996@gnu.ac.kr>
2020-03-17 21:36:10 +0900

    Add Point2d, Point3D

--- src/num.rs
+++ src/num.rs
@@ -1,5 +1,8 @@
 pub type Number = f64;
 
+pub type Point2 = nalgebra::Point2<Number>;
+pub type Point3 = nalgebra::Point3<Number>;
+
 pub type Vector2 = nalgebra::Vector2<Number>;
 pub type Vector3 = nalgebra::Vector3<Number>;
 
